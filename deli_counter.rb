013ec713fe768bd77cf6a832ef7e4027ed02f3eb
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-12-06 00:31:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,11 @@
-# Write your code here.
+#Take in the number
+
+
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
-
-end
 
 def line(katz_deli)
   if katz_deli.length == 0
@@ -23,8 +17,14 @@ def line(katz_deli)
       message += " #{index.to_i+1}. #{value}"
     end
 
-    puts "#{message}"
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
-
-
+end
+  puts "#{message}"
+  end
 end
